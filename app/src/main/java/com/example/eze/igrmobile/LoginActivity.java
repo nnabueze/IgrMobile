7bf454265b67f32b558c2171544fbfd2f57c05a0
Opara Nnabueze <villavelle101@gmail.com>
2017-08-15 00:30:52 +0100

    implementing login logic

--- app/src/main/java/com/example/eze/igrmobile/LoginActivity.java
+++ app/src/main/java/com/example/eze/igrmobile/LoginActivity.java
@@ -1,13 +1,83 @@
 package com.example.eze.igrmobile;
 
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.input_email) EditText emailText;
+    @Bind(R.id.input_password) EditText passwordText;
+    @Bind(R.id.btn_login) Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        if (!validate()){
+            onLoginFail();
+        }
+
+
+    }
+
+    private void onLoginFail() {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+
+        builder.setTitle("Error Message");
+        builder.setMessage("Invalid Email and Password");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        }).show();
+    }
+
+    private boolean validate() {
+        boolean valid = true;
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailText.setError("Enter a valid email address");
+            valid=false;
+        }else{
+            emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 3){
+            passwordText.setError("Password can not be less than 3");
+            valid=false;
+        }else{
+            passwordText.setError(null);
+        }
+        return valid;
     }
 }
